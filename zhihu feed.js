73758ab645e0d4f5e3c50269e995db5095dfa9dc
AaronAshmore <58449124+Eddie1551@users.noreply.github.com>
2020-08-03 16:24:33 +0800

    Update zhihu feed.js

--- zhihu feed.js
+++ zhihu feed.js
@@ -2,7 +2,7 @@ let body = $response.body
 body=JSON.parse(body)
 delete body['ad_info']
 body['data'].forEach((element, index)=> {
-    if(element['author']['name']=="盐选推荐"||element['author']['name']=="盐选科普"||element['author']['name']=="故事档案局"||element['author']['name']=="知乎盐选会员"){ 
+    if(element['author']['name']=="盐选推荐"||element['author']['name']=="故事档案局"){ 
           body['data'].splice(index,1)  
      }
  })
